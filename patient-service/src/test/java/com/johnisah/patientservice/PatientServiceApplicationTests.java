d3dbcadbea6e7a96bb3d5d970e09c43d511aa7e6
johnitopaisah <johnitopaisah@gmail.com>
2025-05-03 15:02:18 +0200

    first commit

---
+++ patient-service/src/test/java/com/johnisah/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnisah.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
